fa306bb3797294bd441de360ed14ac7f0308c804
mneedham <mark.needham@finbourne.com>
2021-03-30 14:18:40 +0100

    update fields returned

--- getting_started.py
+++ getting_started.py
@@ -16,10 +16,10 @@ api_client = api_factory.api_client
 instruments_api = api_factory.build(lusid.api.InstrumentsApi)
 tx_portfolios_api = api_factory.build(lusid.api.TransactionPortfoliosApi)
 
-# Create a portolio
+# Create a portfolio
 
 # You need to set the timezone or it will throw an error
-portfolio_creation_date = datetime.datetime(2021, 3, 29, tzinfo=pytz.utc)
+portfolio_creation_date = datetime.datetime(2021, 3, 20, tzinfo=pytz.utc)
 scope = "GettingStartedScope"
 guid = uuid.uuid4()
 
@@ -43,26 +43,53 @@ figis_to_create = {
     ) for figi, name in [("BBG000C6K6G9", "VODAFONE GROUP PLC"), ("BBG000C04D57", "BARCLAYS PLC")]
 }
 
-instruments = instruments_api.upsert_instruments(request_body=figis_to_create).values
+instruments_api.upsert_instruments(request_body=figis_to_create)
 
-vodafone = [instruments[key].lusid_instrument_id
-            for key in instruments.keys()
-            if instruments[key].name == "VODAFONE GROUP PLC"][0]
+instruments = {
+    value.name: value.lusid_instrument_id
+    for _, value in instruments_api.get_instruments(identifier_type="Figi",
+                                                    request_body=list(figis_to_create.keys())).values.items()
+}
+inverted_instruments = {v: k for k, v in instruments.items()}
 
 # Add transactions
+vodafone_id = instruments["VODAFONE GROUP PLC"]
 tx1 = models.TransactionRequest(
     transaction_id=f"Transaction-{uuid.uuid4()}",
     type="StockIn",
-    instrument_identifiers={"Instrument/default/LusidInstrumentId": vodafone},
-    transaction_date=datetime.datetime(2021, 3, 29, tzinfo=pytz.utc),
-    settlement_date=datetime.datetime(2021, 3, 29, tzinfo=pytz.utc),
+    instrument_identifiers={"Instrument/default/LusidInstrumentId": vodafone_id},
+    transaction_date=datetime.datetime(2021, 3, 27, tzinfo=pytz.utc),
+    settlement_date=datetime.datetime(2021, 3, 28, tzinfo=pytz.utc),
     units=100,
     transaction_price=models.TransactionPrice(price=103),
     total_consideration=models.CurrencyAndAmount(amount=103 * 100, currency="GBP"),
     source="Broker"
 )
+tx2 = models.TransactionRequest(
+    transaction_id=f"Transaction-{uuid.uuid4()}",
+    type="StockIn",
+    instrument_identifiers={"Instrument/default/LusidInstrumentId": vodafone_id},
+    transaction_date=datetime.datetime(2021, 3, 29, tzinfo=pytz.utc),
+    settlement_date=datetime.datetime(2021, 3, 29, tzinfo=pytz.utc),
+    units=500,
+    transaction_price=models.TransactionPrice(price=98),
+    total_consideration=models.CurrencyAndAmount(amount=98 * 500, currency="GBP"),
+    source="Broker"
+)
 
-tx_portfolios_api.upsert_transactions(scope=scope, code=portfolio_code, transaction_request=[tx1])
+tx_portfolios_api.upsert_transactions(scope=scope, code=portfolio_code, transaction_request=[tx1, tx2])
 
+# Get transactions
 transactions = tx_portfolios_api.get_transactions(scope=scope, code=portfolio_code)
-print("transactions:", transactions)
+print("Transactions:")
+for transaction in transactions.values:
+    print(inverted_instruments[transaction.instrument_uid],
+          f"{transaction.units} units @ {transaction.transaction_price.price}")
+
+# Get holdings
+holdings_response = tx_portfolios_api.get_holdings(scope=scope, code=portfolio_code,
+                                                   property_keys=["Instrument/default/Name"]).values
+
+print("Holdings:")
+for holding in holdings_response:
+    print(inverted_instruments[holding.instrument_uid], holding.units, holding.cost.amount)
